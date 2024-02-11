a7b9d4de74660f768411123f5697c80b08397e95
Artur Teodorowicz <teodorowiczartur@gmail.com>
2024-02-11 21:10:44 +0100

    Add refund feature

--- ios/Classes/SwiftIosOpenSubscriptionsSettingsPlugin.swift
+++ ios/Classes/SwiftIosOpenSubscriptionsSettingsPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 import StoreKit
 
 public class SwiftIosOpenSubscriptionsSettingsPlugin: NSObject, FlutterPlugin {
-  @available(iOS 15.0, *)
+    @available(iOS 15.0, *)
     func showCancelSubscriptionModal() async {
         if #available(iOS 15.0, *) {
             if let window = await UIApplication.shared.connectedScenes.first as? UIWindowScene {
@@ -16,17 +16,43 @@ public class SwiftIosOpenSubscriptionsSettingsPlugin: NSObject, FlutterPlugin {
         }
     }
 
+    @available(iOS 15.0, *)
+    func initiateRefundRequest(for transactionId: UInt64) async {
+        if let window = await UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                do {
+                    try await StoreKit.Transaction.beginRefundRequest(for: transactionId, in: window)
+                } catch {
+                    
+                }
+            }
+    }
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if #available(iOS 15.0, *) {
-            Task {
-                await self.showCancelSubscriptionModal()
+            switch call.method {
+            case "showCancelSubscriptionModal":
+                Task {
+                    await self.showCancelSubscriptionModal()
+                }
+            case "initiateRefundRequest":
+            if let args = call.arguments as? [String: Any], 
+               let transactionIdString = args["transactionId"] as? String,
+               let transactionId = UInt64(transactionIdString) {
+                Task {
+                    await self.initiateRefundRequest(for: transactionId)
+                }
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid or missing transactionId", details: nil))
+            }
+            default:
+                result(FlutterMethodNotImplemented)
             }
         }
     }
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ios_open_subscriptions_settings", binaryMessenger: registrar.messenger())
-    let instance = SwiftIosOpenSubscriptionsSettingsPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "ios_open_subscriptions_settings", binaryMessenger: registrar.messenger())
+        let instance = SwiftIosOpenSubscriptionsSettingsPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 }
--- lib/ios_open_subscriptions_settings.dart
+++ lib/ios_open_subscriptions_settings.dart
@@ -3,14 +3,22 @@ import 'package:flutter/services.dart';
 
 class IosOpenSubscriptionsSettings {
   // Static constant variable to initialize MethodChannel of 'ios_open_subscriptions_settings'.
-  static const MethodChannel _channel = MethodChannel('ios_open_subscriptions_settings');
+  static const MethodChannel _channel =
+      MethodChannel('ios_open_subscriptions_settings');
 
   /// Opening subscriptions settings
-  static Future<void> openSubscriptionsSettings({bool asAnotherTask = false}) async {
+  static Future<void> openSubscriptionsSettings(
+      {bool asAnotherTask = false}) async {
     _channel.invokeMethod('openSettingsSubscriptions', {
       'asAnotherTask': asAnotherTask,
     });
   }
+
+  static Future<void> initiateRefundRequest({
+    required String transactionId,
+  }) async {
+    _channel.invokeMethod('initiateRefundRequest', {
+      'transactionId': transactionId,
+    });
+  }
 }
-
-
